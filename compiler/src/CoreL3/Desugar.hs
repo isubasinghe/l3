f6a99d889fcc76ac960d1cb000b6525c574d23a3
Isitha Subasinghe <isubasinghe@student.unimelb.edu.au>
2022-10-04 00:57:27 +0200

    desugar

--- compiler/src/CoreL3/Desugar.hs
+++ compiler/src/CoreL3/Desugar.hs
@@ -4,9 +4,9 @@ module CoreL3.Desugar where
 
 import Control.Monad.State
 import CoreL3.AST
-import qualified Data.DList as DList
+import qualified Data.DList as L
 import qualified Data.Map.Strict as M
-import Data.Text
+import Data.Text (Text)
 
 data Stack a
   = Nil
@@ -14,10 +14,17 @@ data Stack a
 
 type DesugarState = State (Stack Int)
 
-freshVariable :: DesugarState Text
-freshVariable = do
-  s <- get
-  undefined
+class Desugar a b where
+  desugar :: a -> DesugarState b
 
-desugarProgram :: Program -> [Expr]
-desugarProgram p = undefined
+pushNewFrame :: DesugarState ()
+pushNewFrame = undefined
+
+popFrame :: DesugarState (Stack Int)
+popFrame = undefined
+
+freshVariable :: DesugarState Text
+freshVariable = undefined
+
+instance Desugar Program [Expr] where
+  desugar (Program p e) = desugar (p, e)
