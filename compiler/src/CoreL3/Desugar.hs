b5e48f1a7dd24987442c29752bec5b9af30f58bb
Isitha Subasinghe <isubasinghe@student.unimelb.edu.au>
2022-10-04 14:51:09 +0200

    added desugar

--- compiler/src/CoreL3/Desugar.hs
+++ compiler/src/CoreL3/Desugar.hs
@@ -2,9 +2,11 @@
 
 module CoreL3.Desugar where
 
-import Control.Monad.State
+import Control.Monad.Except
+import Control.Monad.State.Strict
 import CoreL3.AST
 import qualified Data.DList as L
+import Data.List (isPrefixOf)
 import qualified Data.Map.Strict as M
 import Data.Text (Text)
 
@@ -12,7 +14,11 @@ data Stack a
   = Nil
   | Cons a (Stack a)
 
-type DesugarState = State (Stack Int)
+data DesugarError = ReservedVariablePattern (Maybe Text)
+  | ExpectedExpression
+  deriving (Show, Eq)
+
+type DesugarState = ExceptT DesugarError (State (Stack Int))
 
 class Desugar a b where
   desugar :: a -> DesugarState b
@@ -26,5 +32,32 @@ popFrame = undefined
 freshVariable :: DesugarState Text
 freshVariable = undefined
 
-instance Desugar Program [Expr] where
-  desugar (Program p e) = desugar (p, e)
+instance Desugar Program Expr where
+  desugar (Program p e) = desugar (p ++ [PExpr e])
+
+instance Desugar ProgramItems Expr where
+  desugar [e] = case e of
+    (PExpr e) -> pure e
+    _ -> throwError ExpectedExpression
+  desugar (p : ps) = case p of
+    (PDefinition p'@(Def n e)) -> do
+      e' <- desugar e
+      ps' <- desugar ps
+      let ret = ELet $ Let [(n, e')] [ps']
+      pure ret
+    (PDefinition (DefRec n e)) -> do
+      e' <- desugar e
+      ps' <- desugar ps
+      pure (ELet $ LetRec [(n, e')] [ps'])
+    (PExpr e) -> do
+      e' <- desugar e
+      pure $ EBegin (Begin [e'])
+
+instance Desugar Expr Expr where
+  desugar (EBegin (Begin (e : es))) = do
+    v <- freshVariable
+    e' <- desugar (EBegin (Begin es))
+    pure (ELet (Let [] [e']))
+
+instance Desugar Fun Fun where
+  desugar e = undefined
