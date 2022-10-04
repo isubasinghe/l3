be1278657aff504431000927b8ec1b90577009af
Isitha Subasinghe <isubasinghe@student.unimelb.edu.au>
2022-10-04 14:59:32 +0200

    added -Wall

--- compiler/src/CoreL3/Desugar.hs
+++ compiler/src/CoreL3/Desugar.hs
@@ -1,13 +1,16 @@
 {-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE MultiParamTypeClasses #-}
+{-# LANGUAGE TypeSynonymInstances #-}
+{-# LANGUAGE FlexibleInstances #-}
 
 module CoreL3.Desugar where
 
 import Control.Monad.Except
 import Control.Monad.State.Strict
 import CoreL3.AST
-import qualified Data.DList as L
-import Data.List (isPrefixOf)
-import qualified Data.Map.Strict as M
+-- import qualified Data.DList as L
+-- import Data.List (isPrefixOf)
+-- import qualified Data.Map.Strict as M
 import Data.Text (Text)
 
 data Stack a
@@ -36,11 +39,12 @@ instance Desugar Program Expr where
   desugar (Program p e) = desugar (p ++ [PExpr e])
 
 instance Desugar ProgramItems Expr where
+  desugar [] = throwError ExpectedExpression
   desugar [e] = case e of
-    (PExpr e) -> pure e
+    (PExpr pe) -> pure pe
     _ -> throwError ExpectedExpression
   desugar (p : ps) = case p of
-    (PDefinition p'@(Def n e)) -> do
+    (PDefinition (Def n e)) -> do
       e' <- desugar e
       ps' <- desugar ps
       let ret = ELet $ Let [(n, e')] [ps']
