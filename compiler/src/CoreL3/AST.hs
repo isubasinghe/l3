712b24ea491465d0f5458fe966c110b3c04e41e5
Isitha Subasinghe <isubasinghe@student.unimelb.edu.au>
2022-10-05 03:20:20 +0200

    reduced corel3

--- compiler/src/CoreL3/AST.hs
+++ compiler/src/CoreL3/AST.hs
@@ -4,49 +4,12 @@ import Data.Text (Text)
 
 type Ident = Text
 
-data Program = Program ProgramItems Expr
-  deriving (Show, Eq)
-
-data ProgramItem
-  = PDefinition Definition
-  | PExpr Expr
-  deriving (Show, Eq)
-
-type ProgramItems = [ProgramItem]
-
-data Definition
-  = Def Ident Expr
-  | DefRec Ident Fun
-  deriving (Show, Eq)
-
-data Fun = Fun [Ident] [Expr]
-  deriving (Show, Eq)
-
 data Let
   = Let [(Ident, Expr)] [Expr]
   | LetStar [(Ident, Expr)] [Expr]
-  | LetRec [(Ident, Fun)] [Expr]
-  deriving (Show, Eq)
-
-data Rec = Rec Ident [(Ident, Expr)] [Expr]
-  deriving (Show, Eq)
-
-data Begin = Begin [Expr]
   deriving (Show, Eq)
 
 data If = If Expr Expr (Maybe Expr)
-  deriving (Show, Eq)
-
-data Cond = Cond [(Expr, [Expr])] -- > 0
-  deriving (Show, Eq)
-
-data And = And [Expr]
-  deriving (Show, Eq)
-
-data Or = Or [Expr]
-  deriving (Show, Eq)
-
-data Not = Not Expr
   deriving (Show, Eq)
 
 data App = App [Expr]
@@ -70,15 +33,8 @@ data Prim = Prim PrimOp [Expr]
   deriving (Show, Eq)
 
 data Expr
-  = EFun Fun
-  | ELet Let
-  | ERec Rec
-  | EBegin Begin
+  = ELet Let
   | EIf If
-  | ECond Cond
-  | EAnd And
-  | EOr Or
-  | ENot Not
   | EApp App
   | EPrim Prim
   | EIdent Ident
